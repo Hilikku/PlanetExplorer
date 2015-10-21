211471b71676388b8dbe2c062b809c4e1b0cbbb1
somename <someemail>
2015-10-21 15:21:49 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
@@ -4,16 +4,30 @@ package org.unioulu.tol.sqat2015.planetExplorer;
 // ID:
 // Finish time:
 public class PlanetExplorer {
+	private int x;
+	private int y;
+	private String obstacles;
+
+
 	public PlanetExplorer(int x, int y, String obstacles){
+		this.setX(x);
+		this.setY(y);
+		this.setObstacles(obstacles);
+		
+		
+		
 	/*	x and y represent the size of the grid.
-	 *  Obstacles is a String formatted as follows: "(obs1_x,obs1_y)(obs2_x,obs2_y)...(obsN_x,obsN_y)" with no white spaces. 
+	 *  Obstacles is a String formatted as follows: "(obs1_x,obs1_y)(obs2_x,obs2_y)...(obsN_x,obsN_y)"
+	 *   with no white spaces. 
 	 *  
 		Example use:
-		PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)")  //A 100x100 grid with two obstacles at coordinates (5,5) and (7,8) 
+		PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)")  //A 100x100 grid with two
+		 obstacles at coordinates (5,5) and (7,8) 
 	 */
 	}
 	
 	public String executeCommand(String command){
+		
 		
 		/* The command string is composed of "f" (forward), "b" (backward), "l" (left) and "r" (right)
 		 * Example: 
@@ -21,10 +35,39 @@ public class PlanetExplorer {
 		 * The explorer is given the commands "ffrff" and should end up at (2, 2) facing East.
 		 
 		 * The return string is in the format: "(pos_x,pos_y,facing)(obs1_x,obs1_y)(obs2_x,obs2_y)..(obsN_x,obsN_y)" 
-		 * Where pos_x and pos_y are the final coordinates, facing is the current direction the explorer is pointing to (N,S,W,E).
-		 * The return string should also contain a list of coordinates of the encountered obstacles. No white spaces.
+		 * Where pos_x and pos_y are the final coordinates, facing is the current direction the explorer
+		 *  is pointing to (N,S,W,E).
+		 * The return string should also contain a list of coordinates of the encountered obstacles.
+		 *  No white spaces.
 		 */
 		
 		return null;
 	}
+	
+	
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public String getObstacles() {
+		return obstacles;
+	}
+
+	public void setObstacles(String obstacles) {
+		this.obstacles = obstacles;
+	}
+	
+	
 }

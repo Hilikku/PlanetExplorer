bce30624fdf87566306279dba7f6ec13879bcfc5
somename <someemail>
2015-10-21 16:25:28 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/Explorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/Explorer.java
@@ -1,5 +1,45 @@
 package org.unioulu.tol.sqat2015.planetExplorer;
 
 public class Explorer {
-
+	
+	private String facing;
+	
+//	public Explorer(String command) {
+	
+//		int face = 0;
+//		//String facing;
+//		for(int i=0; i < command.length(); i++){
+//		if(command.charAt(i) == 114) {
+//			face++;
+//		}
+//		if (command.charAt(i) == 108) {
+//			face--;
+//		}
+//			if(face < -3 || face > 3) {
+//				face =0;
+//			}
+//		}
+//		switch (face) {
+//		case 0: facing = "N";
+//			break;
+//		case 1: facing = "E";
+//			break;
+//		case 2: facing = "S";
+//			break;
+//		case 3: facing = "W";
+//			break;
+//		case -1: facing = "W";
+//			break;
+//		case -2: facing = "S";
+//			break;
+//		case -3: facing = "E";
+//			break;
+//			
+//		}
+//	
+//	}
+//	public String Position(String facing) {
+//		if
+//		
+//	}
 }
